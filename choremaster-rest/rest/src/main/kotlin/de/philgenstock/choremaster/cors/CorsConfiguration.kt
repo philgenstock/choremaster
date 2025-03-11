46b98af4b8e0493af6d732f4544a7435f798fb44
Patrick Hilgenstock <patrick.hilgenstock@hermesworld.com>
2025-03-11 16:10:00 +0100

    Configure cors to allow requests from dev env

---
+++ choremaster-rest/rest/src/main/kotlin/de/philgenstock/choremaster/cors/CorsConfiguration.kt
@@ -0,0 +1,19 @@
+package de.philgenstock.choremaster.cors
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfiguration {
+
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer = object : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+        }
+    }
+}
