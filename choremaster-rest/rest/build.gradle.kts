4d94954973b2dadf3d82cc9067b0af07581114ff
Patrick Hilgenstock <patrick.hilgenstock@hermesworld.com>
2025-03-11 14:23:29 +0100

    Cleanup gradle build files

--- choremaster-rest/rest/build.gradle.kts
+++ choremaster-rest/rest/build.gradle.kts
@@ -1,3 +1,13 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.4.3"
+}
+
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
