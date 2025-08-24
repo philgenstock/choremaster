5aaedb64f3d358f6cfefe6a3428cf5213b9b1f17
Patrick Hilgenstock <patrick.hilgenstock@hermesworld.com>
2025-08-24 16:51:11 +0200

    Initial commit

---
+++ rest/src/main/java/de/philgenstock/choremaster/ChoremasterApplication.java
@@ -0,0 +1,12 @@
+package de.philgenstock.choremaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoremasterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChoremasterApplication.class, args);
+  }
+}
