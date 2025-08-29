5b7bacf942f603f46b9fbcad10ec52da54d69555
Patrick Hilgenstock <patrick.hilgenstock@hermesworld.com>
2025-08-29 08:07:58 +0200

    Cleanup

--- rest/src/main/java/de/philgenstock/choremaster/household/HouseHoldRepository.java
+++ rest/src/main/java/de/philgenstock/choremaster/household/HouseHoldRepository.java
@@ -1,9 +1,5 @@
 package de.philgenstock.choremaster.household;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface HouseHoldRepository extends CrudRepository<HouseHoldEntity, Long> {
-
-  List<HouseHoldEntity> findAll();
-}
+public interface HouseHoldRepository extends CrudRepository<HouseHoldEntity, Long> {}
--- rest/src/main/java/de/philgenstock/choremaster/household/HouseHoldService.java
+++ rest/src/main/java/de/philgenstock/choremaster/household/HouseHoldService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class HouseHoldService {
 
-  private final HouseHoldRepository houseHoldRepository;
   private final HouseHoldConvertService houseHoldConvertService;
   private final UserService userService;
 
