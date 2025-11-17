e9c3e625c4865f94811fd424231494a1a27eea75
Patrick Hilgenstock <p.hilgenstock@reply.de>
2025-11-17 11:48:57 +0100

    Add lombok annotation processors

--- choremaster-backend/build.gradle.kts
+++ choremaster-backend/build.gradle.kts
@@ -25,7 +25,13 @@ dependencies {
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
-    implementation("org.projectlombok:lombok:1.18.42")
+    val lombokVersion = "1.18.42"
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
