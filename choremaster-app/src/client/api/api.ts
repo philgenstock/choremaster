f73c20b9f0660127bb24fa7f0d946ebbe54da9fb
Patrick Hilgenstock <p.hilgenstock@reply.de>
2025-11-26 14:47:51 +0100

    generate client

--- choremaster-app/src/client/api/api.ts
+++ choremaster-app/src/client/api/api.ts
@@ -1,7 +1,9 @@
 export * from './choreController.service';
 import { ChoreControllerService } from './choreController.service';
+export * from './choreExecutionController.service';
+import { ChoreExecutionControllerService } from './choreExecutionController.service';
 export * from './householdController.service';
 import { HouseholdControllerService } from './householdController.service';
 export * from './userController.service';
 import { UserControllerService } from './userController.service';
-export const APIS = [ChoreControllerService, HouseholdControllerService, UserControllerService];
+export const APIS = [ChoreControllerService, ChoreExecutionControllerService, HouseholdControllerService, UserControllerService];
