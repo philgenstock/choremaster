b10569160bce4c435569d0ef210dceb9aedc2dc6
Patrick Hilgenstock <p.hilgenstock@reply.de>
2025-12-15 08:51:29 +0100

    Show chore information in chore-detail

--- choremaster-app/src/app/pages/chore-detail/chore-detail.ts
+++ choremaster-app/src/app/pages/chore-detail/chore-detail.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChoreControllerService, ChoreDto, ChoreExecutionControllerService, ChoreExecutionDto } from '../../../client';
 import { T } from '@angular/cdk/keycodes';
-import { MatCard } from "@angular/material/card";
+import { MatCard, MatCardTitle } from "@angular/material/card";
+import { ChoreService } from '../../service/chore-service';
 
 @Component({
   selector: 'app-chore-detail',
-  imports: [MatCard],
+  imports: [MatCard, MatCardTitle],
   standalone: true,
   templateUrl: './chore-detail.html',
   styleUrl: './chore-detail.scss',
@@ -14,13 +15,19 @@ import { MatCard } from "@angular/material/card";
 export class ChoreDetail implements OnInit {
   route = inject(ActivatedRoute);
   choreExecutions = signal<ChoreExecutionDto[] | null>(null)
+  chore = signal<ChoreDto | null>(null)
 
   choreExecutionControllerService = inject(ChoreExecutionControllerService)
+  choreService = inject(ChoreService)
   ngOnInit(): void {
     const choreId = this.route.snapshot.paramMap.get('choreId');
     if(choreId) {
-      this.choreExecutionControllerService.getExecutionsByChore(Number(choreId))
+      const choreIdNumber = Number(choreId)
+      this.choreExecutionControllerService.getExecutionsByChore(choreIdNumber)
         .subscribe(executions => this.choreExecutions.set(executions))
+
+      this.choreService.getChore(choreIdNumber)
+        .subscribe(chore => this.chore.set(chore))
       
     }
   }
